3c07e8d96dc5bf704e7d72654d4424da31c60f2f
Justyna Marciniak <justy.marciniak@gmail.com>
2020-08-01 21:53:57 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hi, I'm a React App</h1>
+    </div>
+  );
+}
+
+export default App;
