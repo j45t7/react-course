e5877e6e65759e3486ebfdda2aa930b8673e7d6c
Justyna Marciniak <justy.marciniak@gmail.com>
2020-08-02 13:16:31 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hi, I'm a React App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Maniek', age: 26},
+      {name: 'Maciek', age: 25},
+
+    ]
+  }
+
+  render(){
+    return (
+      <div className="App">
+       <h1>Hi, I'm a React App</h1>
+       <button>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
+       <Person ame={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+
+  }
 }
 
 export default App;
+ 
